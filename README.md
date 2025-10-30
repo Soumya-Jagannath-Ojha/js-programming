9acaa0e01cb368f56367a3f273bd2d11a823525f
Soumya-Jagannath-Ojha <soumyajagannathojha7@gmail.com>
2025-10-30 08:58:38 +0530

    pattern and number problems

---
+++ README.md
@@ -0,0 +1,16 @@
+# 💻 JavaScript Practice Problems
+
+This repository contains my **JavaScript practice problems**, including **patterns, numbers, strings, and arrays**.  
+It’s aimed at strengthening my logic-building, problem-solving, and coding skills using pure JavaScript.
+
+---
+
+## 🚀 Topics Covered
+
+### 🟢 Pattern Problems
+- Print triangle, pyramid, and diamond patterns  
+- Star (`*`) and number pattern generation  
+ 
+
+### 🔢 Number Problems
+- Number Series 
---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+// for(let i=1; i<=100; i++){
+ 
+//     if(i=== 3){
+//         continue;
+//     }
+//     if(i > 5 && i-5 == 3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let x = 1;
+// for(let i=1; i<=100; i++){
+//     if(i == x){
+//         x= 2*x +1;
+//         console.log(i);
+//     }
+// }
+
+// 1,3,5,15,31
+
+
+
+//!  29-10-2025 
+// for(var i=1; i<=5; i++){
+//     var x = 1;
+//     x++;
+//     console.log(x);
+// }
+
+
+// for(var i=1; i<=5; i++){
+//     console.log("i:",i)
+//     for(var j=1; j<=5; j++){
+//         console.log("j:",j)
+//     }
+//     console.log("-------")
+// }
+
+// 1,11,111,1111
+// for(var i=1; i<=5; i++){
+//     var res = "";
+//     for(var j=1; j<=i; j++){
+//         res += "1"
+//     }
+//     console.log(res)
+// }
